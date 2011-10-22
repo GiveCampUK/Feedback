9113152c5ef3f0af32a3f2eda606aad04e23cabb
Phil Pursglove <phil@philippursglove.com>
2011-10-22 17:08:45 +0100

    initial commit

---
+++ SceneAndHeardFeedback/Controllers/FeedbackController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SceneAndHeardFeedback.Models;
+using EventbriteNET.Entities;
+using EventbriteNET;
+
+namespace SceneAndHeardFeedback.Controllers
+{
+    public class FeedbackController : Controller
+    {
+        private EventbriteNET.EventbriteContext context;
+        //
+        // GET: /Feedback/
+
+        public ActionResult Index()
+        {
+            context = new EventbriteContext("27SKVYI2C5KFLVID6W", null);
+
+            Organizer sceneandheard = new Organizer(1593730564, context);
+            var eventList = sceneandheard.Events.Select(@event => @event.Value).ToList();
+
+
+            return View(eventList);
+        }
+
+        public ActionResult LeaveFeedback()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult LeaveFeedback(Feedback feedback)
+        {
+            if (ModelState.IsValid)
+            {
+            }
+            return RedirectToAction("Index", "Feedback");
+        }
+
+    }
+}
---
+++ SceneAndHeardFeedback/Models/Feedback.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SceneAndHeardFeedback.Models
+{
+    public class Feedback
+    {
+        public int Id { get; set; }
+        [DataType(DataType.MultilineText)]
+        [Required]
+        public string FeedbackText { get; set; }
+        public DateTime FeedbackLeft { get; set; }
+        [Required]
+        public string eventBriteId { get; set; }
+        [Required]
+        public string ContactName { get; set; }
+        [DataType(DataType.EmailAddress)]
+        public string ContactEmailAddress { get; set; }
+        public string OrganisationName { get; set; }
+
+    }
+}
